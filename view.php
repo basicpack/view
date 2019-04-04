15daeb6e90955fd74de9a94818689249eec64221
Andeson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-04 09:28:24 -0300

    Update view.php

--- view.php
+++ view.php
@@ -3,6 +3,9 @@
 //24 de agosto de 2018
 
 function view($name,$data=null,$print=true){
+    if($name=='404'){
+        header('HTTP/1.0 404 Not Found');   
+    }
     $filename=ROOT.'view/'.$name.'.php';
     if(file_exists($filename)){
         if(is_array($data)){
