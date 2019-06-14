1362e0b182cc41d700fbbea337ac5db4281123f1
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-06-14 16:15:56 -0300

    M  view.php

--- view.php
+++ view.php
@@ -4,17 +4,17 @@
 
 function view($name,$data=null,$print=true){
     if($name=='404'){
-        header('HTTP/1.0 404 Not Found');   
+        header('HTTP/1.0 404 Not Found');
     }
     $filename=ROOT.'view/'.$name.'.php';
+    $name2=explode('/',$name);
+    $repoName=@$name2[0];
+    $viewName=@$name2[1];
+    $filename2=ROOT.$repoName.'/view/'.$viewName.'.php';
     if(file_exists($filename)){
-        if(is_array($data)){
-            extract($data);
-        }
-        ob_start();
-        require $filename;
-        $output=ob_get_contents();
-        ob_end_clean();
+        $output=getViewOutput($filename,$data);
+    }elseif(file_exists($filename2)){
+        $output=getViewOutput($filename2,$data);
     }else{
         $output='<b>Erro:</b><br>'.PHP_EOL.'view <b>'.$name.'</b> not found';
     }
@@ -23,4 +23,14 @@ function view($name,$data=null,$print=true){
     }else{
         return $output;
     }
+}
+function getViewOutput($filename,$data){
+    if(is_array($data)){
+        extract($data);
+    }
+    ob_start();
+    require $filename;
+    $output=ob_get_contents();
+    ob_end_clean();
+    return $output;
 }
