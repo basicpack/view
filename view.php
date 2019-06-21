57b5a3cd84e192837b24da16e3115bdf5d718f7d
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-06-20 21:34:35 -0300

    M  view.php

--- view.php
+++ view.php
@@ -3,20 +3,27 @@
 //24 de agosto de 2018
 
 function view($name,$data=null,$print=true){
-    if($name=='404'){
+    $nameArr=explode('/',$name);
+    end($nameArr);
+    $key=key($nameArr);
+    if($name=='404' OR $nameArr[$key]=='404'){
         header('HTTP/1.0 404 Not Found');
     }
     $filename=ROOT.'view/'.$name.'.php';
     $name2=explode('/',$name);
     $repoName=@$name2[0];
-    $viewName=@$name2[1];
+    unset($name2[0]);
+    $viewName=implode('/',$name2);
     $filename2=ROOT.$repoName.'/view/'.$viewName.'.php';
     if(file_exists($filename)){
         $output=getViewOutput($filename,$data);
     }elseif(file_exists($filename2)){
         $output=getViewOutput($filename2,$data);
     }else{
-        $output='<b>Erro:</b><br>'.PHP_EOL.'view <b>'.$name.'</b> not found';
+        $output='<p><b>Erro:</b><br>'.PHP_EOL.'views ';
+        $output.='<b>'.$filename.'</b> and ';
+        $output.='<b>'.$filename2.'</b>';
+        $output.=' not found';
     }
     if($print){
         print $output;
